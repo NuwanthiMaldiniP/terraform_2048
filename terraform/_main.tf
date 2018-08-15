9fb7f15d05a72590f42596310555793dc873fd5d
vmaldnu <1990Tedy@>
2018-08-15 13:52:15 +0530

    Initial Commit

---
+++ terraform/_main.tf
@@ -0,0 +1,11 @@
+        provider "aws" {
+        region     = "ap-south-1"
+        access_key = "AKIAIRQNZ6AVSC3QLRVA"
+        secret_key = "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"
+        }
+
+        resource "aws_key_pair" "key" {
+                key_name   = "${var.key_name}"
+                public_key = "${file("staging_key.pub")}"
+                }
+
