5d9fe8c151d949f9b31295e0866a2dca673381d1
vmaldnu <1990Tedy@>
2018-08-15 13:54:57 +0530

    Initial Commit

--- terraform/_main.tf
+++ terraform/_main.tf
@@ -6,6 +6,6 @@
 
         resource "aws_key_pair" "key" {
                 key_name   = "${var.key_name}"
-                public_key = "${file("staging_key.pub")}"
+                public_key = "${file("main_key.pub")}"
                 }
 
