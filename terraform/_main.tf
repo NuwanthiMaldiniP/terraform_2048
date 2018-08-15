9d3288b51afac3499931bcc5ae25ce39794ce498
vmaldnu <1990Tedy@>
2018-08-15 16:52:15 +0530

    Initial Commit

--- terraform/_main.tf
+++ terraform/_main.tf
@@ -1,7 +1,5 @@
         provider "aws" {
         region     = "us-west-1a"
-        access_key = "AKIAIRQNZ6AVSC3QLRVA"
-        secret_key = "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"
         }
 
         resource "aws_key_pair" "key" {
