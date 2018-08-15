e655d3bfac960da98b915485a555b908c3525662
vmaldnu <1990Tedy@>
2018-08-15 17:20:32 +0530

    Initial Commit

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -4,7 +4,7 @@ module "networking" {
         vpc_cidr            = "10.0.0.0/16"
         public_subnet_cidr  = "10.0.2.0/24"
         private_subnet_cidr = "10.0.1.0/24"
-        region              = "us-west-1a"
+        region              = "us-west-1"
         availability_zone   = "us-west-1a"
         key_name            = "test"
         }
